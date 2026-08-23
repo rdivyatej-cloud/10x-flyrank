6946a62903103bbadf8e9e1a58ec82860122cd24
Divyatej <rdivyatej@gmail.com>
2026-08-24 00:19:48 +0530

    Prepare project for deployment

--- src/server.js
+++ src/server.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV !== 'test') {
   // Start background job
   startMonthlySummaryJob();
   
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
